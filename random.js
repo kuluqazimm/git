5b37d27e08b06b22481fa3510705dd0027e52b7c
kuluqazimm <azimkhuluqin@gmail.com>
2023-08-08 19:35:38 +0700

    menambahkan fitur perkalian

--- random.js
+++ random.js
@@ -4,5 +4,8 @@ console.log('fungsi pertama');
 const add = ( a, b ) => a + b;
 console.log(add(3,5));
 
-const substract  = ( a, b ) => a - b;
-console.log(substract(5.3));
+const subtract  = ( a, b ) => a - b;
+console.log(subtract(5,3));
+
+const multiply = (a, b) => a * b;
+console.log(multiply(5,6));
