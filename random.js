9153dddfe7906d297b435c368d62942a5f6b12c1
kuluqazimm <azimkhuluqin@gmail.com>
2023-08-09 08:12:32 +0700

    fitur sin matematika

--- random.js
+++ random.js
@@ -4,16 +4,14 @@ console.log('fungsi pertama');
 const add = ( a, b ) => a + b;
 console.log(add(3,5));
 
-<<<<<<< HEAD
 const subtract  = ( a, b ) => a - b;
 console.log(subtract(5,3));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(5,6));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(5,6));
 
 const divide = (a, b) => a / b;
 console.log(divide(12,6));
-=======
-const substract  = ( a, b ) => a - b;
-console.log(substract(5.3));
->>>>>>> parent of 5b37d27 (menambahkan fitur perkalian)
+
+const Mathsin = (Number) => Math.sin(Number);
+console.log(Mathsin(45));
